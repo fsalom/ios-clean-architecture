e1962de04bf3cd40c9ec5ca3f265829a12e5114b
Fernando Salom <Fdosalom@gmail.com>
2022-11-05 11:39:54 +0100

    Add initial project

---
+++ clean/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  clean
+//
+//  Created by Fernando Salom Carratala on 5/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
