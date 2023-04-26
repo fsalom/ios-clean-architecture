a4389d135aba146cfec17cbdc2040bdce7252bb8
Fernando Salom <fer@rudo.es>
2023-04-26 12:30:31 +0200

    Add new cells

---
+++ clean/Presentation/Modules/List/Cell/MarvelCell/MarvelCell.swift
@@ -0,0 +1,37 @@
+//
+//  MarvelCell.swift
+//  clean
+//
+//  Created by Fernando Salom Carratala on 26/4/23.
+//
+
+import UIKit
+
+class MarvelCell: UITableViewCell {
+    @IBOutlet weak var marvelCharacterImage: UIImageView!
+
+    var character: (CharacterProtocol & MarvelCharacterProtocol)! {
+        didSet {
+            setupUI()
+        }
+    }
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    func setupUI() {
+        marvelCharacterImage.layer.masksToBounds = false
+        marvelCharacterImage.clipsToBounds = true
+        marvelCharacterImage.backgroundColor = .white
+        if let urlString = character.image, let url = URL(string: urlString) {
+            marvelCharacterImage.load(url: url)
+        }
+    }
+}
